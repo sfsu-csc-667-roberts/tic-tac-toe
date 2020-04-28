39895cfba9672631c36e05a7bd938f4f21106e9e
John Roberts <roberts.john@gmail.com>
2020-04-27 20:05:29 -0700

    Store encrypted password instead of plain text password (#1)
    * Added bcrypt dependency
    * Added basic registration view and endpoint
    * Added the post register route
    * Changed login flow to use encrypted password

--- db/users.js
+++ db/users.js
@@ -1,10 +1,20 @@
 const db = require('./connection.js')
 
 const create = (email, password) =>
-  db.any("INSERT INTO users (email, password) VALUES($1, $2) RETURNING id, email", [email, password])
+  db.one("INSERT INTO users (email, password) VALUES($1, $2) RETURNING id, email", [email, password])
 
 const findById = id =>
   db.one("SELECT id, email, created_at FROM users WHERE id=$1", [id])
+
+const findPasswordByEmail = email =>
+  db.any("SELECT password FROM users WHERE email=$1", [email])
+    .then(rows => {
+      if (rows.length !== 1) {
+        return Promise.reject(`Credentials invalid.`)
+      } else {
+        return rows[0];
+      }
+    })
 
 const findByEmailAndPassword = (email, password) =>
   db.one("SELECT id, email, created_at FROM users WHERE email=${email} and password=${password}", { email, password })
@@ -12,5 +22,6 @@ const findByEmailAndPassword = (email, password) =>
 module.exports = {
   create,
   findById,
+  findPasswordByEmail,
   findByEmailAndPassword
 }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../authentication')
+const bcrypt = require('bcrypt');
+const Users = require('../db').Users;
 
-router.get('/login', function (request, response) {
+router.get('/login', (request, response) => {
   const message = request.flash('error')
 
   response.render('unauthenticated/login', { message })
@@ -21,8 +23,29 @@ router.get('/logout', (request, response) => {
   response.redirect('/')
 })
 
-router.get('/register', function (_, response) {
-  response.render('unauthenticated/register')
+router.get('/register', (_, response) => {
+  response.render('unauthenticated/register', { message: [] })
+})
+
+router.post('/register', (request, response) => {
+  const { username, password } = request.body
+
+  bcrypt.hash(password, 10)
+    .then(hashedPassword => Users.create(username, hashedPassword))
+    .then(user => {
+      request.login({ id: user.id }, error => {
+        if (error != null) {
+          console.log(error)
+          response.render('unauthenticated/login', { message: ['An error occurred when trying to log you in.'] })
+        } else {
+          response.redirect('/lobby');
+        }
+      })
+    })
+    .catch(error => {
+      console.log(error)
+      response.render('unauthenticated/register', { message: ['Something bad happened.' + error] })
+    })
 })
 
 module.exports = router
